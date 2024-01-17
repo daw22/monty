69bfe6ddce356598d5fe38624c7f3f8ff886193b
Dawit Yifru <dawitxxd@gmail.com>
2024-01-18 00:49:14 +0300

    getline fix

--- monty.h
+++ monty.h
@@ -1,12 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _GNU_SOURCE 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <fcntl.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
